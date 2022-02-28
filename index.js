ba8ba68951a73555b6046861f618dd3e4373e90e
SamanjaCartagena <cart.samanja09@gmail.com>
2022-02-27 19:02:01 -0500

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const brain = require('brain.js');
+var brain = require('brain.js');
 
-const network = new brain.NeuralNetwork();
-network.train([
-    {input:[1,2], output:[1]},//Team 2 wins
-    {input:[1,3], output:[1]}, //Team 3 wins
-    {input:[2,3], output:[0]}, //Team 2 wins
-    {input:[2,4], output:[1]}, //Team 4 wins
-    {input:[1,2], output:[0]},
-    {input:[1,3], output:[0]},
-    {input:[3,4], output:[0]},
+var net = new brain.NeuralNetwork();
 
+net.train([
+    { input: [0,0],output:[0] },
+    {input:[0,1], output:[1]},
+    {input:[1,0], output:[1]},
+    {input:[1,1], output:[0]},
 ]);
 
-const output = network.run([1,4]);
+ var output = net.run([1,0]);
 
-console.log(`Prob: ${output}`);
+ console.log(output);
+
